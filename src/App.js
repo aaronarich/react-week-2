e9f53534ff8b26340058e44f2a8d37a1524859e0
Aaron A. Rich <aaron@aaronarich.com>
2018-02-08 18:12:58 -0500

    WIP - Create searchGiphy to send request and receive JSON to console

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import loader from "./images/loader.svg"
+import loader from "./images/loader.svg";
 
 // This is the Header Component
 const Header = () => (
@@ -8,11 +8,15 @@ const Header = () => (
   </div>
 );
 
-const UserHint = ({loading, hintText}) => (
+const UserHint = ({ loading, hintText }) => (
   <div className="user-hint">
-    { loading ? <img className="block mx-autos" src={loader} alt="Loading Indicator"/> : hintText }
+    {loading ? (
+      <img className="block mx-autos" src={loader} alt="Loading Indicator" />
+    ) : (
+      hintText
+    )}
   </div>
-)
+);
 
 class App extends Component {
   constructor(props) {
@@ -23,13 +27,25 @@ class App extends Component {
     };
   }
 
+  searchGiphy = async searchTerm => {
+    try {
+      const response = await fetch(
+        "https://api.giphy.com/v1/gifs/search?api_key=C2p6jvcOM1cGL0Sip4JoF979vsaeC1LF&q=dog&limit=25&offset=0&rating=G&lang=en"
+      );
+      const data = await response.json();
+
+      console.log(data);
+
+    } catch (error) {}
+  };
+
   // Quick function that logs key presses to console
   handleChange = event => {
     const { value } = event.target;
     this.setState((prevState, props) => ({
       ...prevState,
       searchTerm: value,
-      hintText: `Hit enter to search for ${value}`
+      hintText: value.length > 2 ? `Hit enter to search for ${value}` : ""
     }));
     if (value.length > 2) {
       console.log(event.target.value);
@@ -41,7 +57,7 @@ class App extends Component {
     const { value } = event.target;
 
     if (value.length > 2 && event.key === "Enter") {
-      alert(`search for ${value}`);
+      this.searchGiphy(value);
     }
   };
 
